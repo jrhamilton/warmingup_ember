2d5d61036a765c153e79cf78099940f33521c4f1
Jason Hamilton <earth2hamilton@gmail.com>
2014-05-02 15:52:11 -0700

    Uses store.find in routing

--- js/app.js
+++ js/app.js
@@ -1,25 +1,22 @@
 var App = Ember.Application.create({
   LOG_TRANSITIONS: true
 });
-
 App.Router.map(function() {
   this.route('credits', { path: '/thanks' });
   this.resource('products', function() {
-    this.resource('product', { path: '/:title' });
+    this.resource('product', { path: '/:product_id' });
   });
   this.resource('contacts', function() {
-    this.resource('contact', { path: '/:name' }):
+    this.resource('contact', { path: '/:contact_id' });
   });
 });
-
 App.IndexController = Ember.Controller.extend({
-  productCount: 6,
+  productsCount: 6,
   logo: 'images/logo-small.png',
   time: function() {
     return (new Date()).toDateString();
   }.property()
 });
-
 App.ContactsIndexController = Ember.Controller.extend({
   contactName: 'Anostagia',
   avatar: 'images/avatar.png',
@@ -30,55 +27,62 @@ App.ContactsIndexController = Ember.Controller.extend({
 
 App.ProductsRoute = Ember.Route.extend({
   model: function() {
-    return App.PRODUCTS;
+    return this.store.findAll('product');
   }
 });
-
-App.ProductRoute = Ember.Route.extend({
-  model: function(params) {
-    return App.PRODUCTS.findBy('title', params.title)
-  }
-})
-
 App.ContactsRoute = Ember.Route.extend({
   model: function() {
-    return App.CONTACTS;
+    return this.store.findAll('contact');
+  }
+});
+App.ContactRoute = Ember.Route.extend({
+  model: function(params) {
+    return this.store.find('contact', params.contact_id);
   }
 });
 
-App.ContactRoute = Ember.Route.extend({
-  model: function(params) {
-    return App.CONTACTS.findBy('name', params.name)
-  }
-})
-
-
-App.PRODUCTS = [
+App.ApplicationAdapter = DS.FixtureAdapter.extend();
+App.Product = DS.Model.extend({
+  title: DS.attr('string'),
+  price: DS.attr('number'),
+  description: DS.attr('string'),
+  isOnSale: DS.attr('boolean'),
+  image: DS.attr('string')
+});
+App.Product.FIXTURES = [
   {
+    id: 1,
     title: 'Flint',
     price: 99,
-    description: 'Flint is...',
+    description: 'Flint is a hard, sedimentary cryptocrystalline form of the mineral quartz, categorized as a variety of chert.',
     isOnSale: true,
     image: 'images/products/flint.png'
   },
   {
+    id: 2,
     title: 'Kindling',
     price: 249,
-    description: 'Easily...',
+    description: 'Easily combustible small sticks or twigs used for starting a fire.',
     isOnSale: false,
     image: 'images/products/kindling.png'
   }
 ];
-
-App.CONTACTS = [
+App.Contact = DS.Model.extend({
+  name: DS.attr('string'),
+  about: DS.attr('string'),
+  avatar: DS.attr('string')
+});
+App.Contact.FIXTURES = [
   {
-    name: 'The Dude',
-    avatar: 'images/contacts/giamia.png',
-    about: 'Likes White Russians. But he will never say'
+    id: 1,
+    name: 'Giamia',
+    about: 'Although Giamia came from a humble spark of lightning, he quickly grew to be a great craftsman, providing all the warming instruments needed by those close to him.',
+    avatar: 'images/contacts/giamia.png'
   },
   {
-    name: 'Other Dude',
-    avatar: 'images/contacts/anostagia.png',
-    about: 'Who cares about the other dude'
+    id: 2,
+    name: 'Anostagia',
+    about: 'Knowing there was a need for it, Anostagia drew on her experience and spearheaded the Flint & Flame storefront. In addition to coding the site, she also creates a few products available in the store.',
+    avatar: 'images/contacts/anostagia.png'
   }
 ];
