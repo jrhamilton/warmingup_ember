237e292e38f8518e68bb871fb53816c8394231fc
Jason Hamilton <earth2hamilton@gmail.com>
2014-05-02 16:22:24 -0700

    Adds relations for products to reviews and relations to contact to product

--- js/app.js
+++ js/app.js
@@ -10,6 +10,7 @@ App.Router.map(function() {
     this.resource('contact', { path: '/:contact_id' });
   });
 });
+
 App.IndexController = Ember.Controller.extend({
   productsCount: 6,
   logo: 'images/logo-small.png',
@@ -35,11 +36,6 @@ App.ContactsRoute = Ember.Route.extend({
     return this.store.findAll('contact');
   }
 });
-App.ContactRoute = Ember.Route.extend({
-  model: function(params) {
-    return this.store.find('contact', params.contact_id);
-  }
-});
 
 App.ApplicationAdapter = DS.FixtureAdapter.extend();
 App.Product = DS.Model.extend({
@@ -47,42 +43,69 @@ App.Product = DS.Model.extend({
   price: DS.attr('number'),
   description: DS.attr('string'),
   isOnSale: DS.attr('boolean'),
-  image: DS.attr('string')
+  image: DS.attr('string'),
+  reviews: DS.hasMany('review', { async: true }),
+  crafter: DS.belongsTo('contact', { async: true })
 });
+
 App.Product.FIXTURES = [
-  {
-    id: 1,
-    title: 'Flint',
-    price: 99,
-    description: 'Flint is a hard, sedimentary cryptocrystalline form of the mineral quartz, categorized as a variety of chert.',
-    isOnSale: true,
-    image: 'images/products/flint.png'
-  },
-  {
-    id: 2,
-    title: 'Kindling',
-    price: 249,
-    description: 'Easily combustible small sticks or twigs used for starting a fire.',
-    isOnSale: false,
-    image: 'images/products/kindling.png'
-  }
+ {
+  id: 1,
+  title: 'Flint',
+  price: 99,
+  description: 'Flint is a hard, sedimentary cryptocrystalline form of the mineral quartz, categorized as a variety of chert.',
+  isOnSale: true,
+  image: 'images/products/flint.png',
+  reviews: [100,101],
+  crafter: 200
+ },
+ {
+  id: 2,
+  title: 'Kindling',
+  price: 249,
+  description: 'Easily combustible small sticks or twigs used for starting a fire.',
+  isOnSale: false,
+  image: 'images/products/kindling.png',
+  reviews: [],
+  crafter: 201
+ }
 ];
+
 App.Contact = DS.Model.extend({
   name: DS.attr('string'),
   about: DS.attr('string'),
-  avatar: DS.attr('string')
+  avatar: DS.attr('string'),
+  products: DS.hasMany('product', { async: true })
 });
 App.Contact.FIXTURES = [
   {
-    id: 1,
+    id: 200,
     name: 'Giamia',
     about: 'Although Giamia came from a humble spark of lightning, he quickly grew to be a great craftsman, providing all the warming instruments needed by those close to him.',
-    avatar: 'images/contacts/giamia.png'
+    avatar: 'images/contacts/giamia.png',
+    products: [1]
   },
   {
-    id: 2,
+    id: 201,
     name: 'Anostagia',
     about: 'Knowing there was a need for it, Anostagia drew on her experience and spearheaded the Flint & Flame storefront. In addition to coding the site, she also creates a few products available in the store.',
-    avatar: 'images/contacts/anostagia.png'
+    avatar: 'images/contacts/anostagia.png',
+    products: [2]
+  }
+];
+
+App.Review = DS.Model.extend({
+  text: DS.attr('string'),
+  reviewedAt: DS.attr('date'),
+  product: DS.belongsTo('product')
+});
+App.Review.FIXTURES = [
+  {
+    id: 100,
+    text: "Started a fire in no time!"
+  },
+  {
+    id: 101,
+    text: "Not the brightest flame, but warm!"
   }
 ];
